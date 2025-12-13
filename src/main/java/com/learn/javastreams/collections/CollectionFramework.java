050efb23f541ed0d7bbed664d8a71b5c3555e17a
Laxminarayana Alekha <laxmishalekha123@gmail.com>
2025-12-13 13:24:17 +0530

    hashcode override for uniqueness for custom classes

--- src/main/java/com/learn/javastreams/collections/CollectionFramework.java
+++ src/main/java/com/learn/javastreams/collections/CollectionFramework.java
@@ -1,7 +1,6 @@
 package com.learn.javastreams.collections;
 
-import org.springframework.scheduling.config.DelayedTask;
-
+import java.io.Serializable;
 import java.util.*;
 import java.util.concurrent.*;
 
@@ -19,6 +18,28 @@ public class CollectionFramework {
         List<Integer> list2 = new Vector<>();                  // synchronized ArrayList (thread-safe, legacy)
         List<Integer> list3 = new Stack<>();                   // thread-safe LIFO stack (legacy, extends Vector)
         List<Integer> list4 = new CopyOnWriteArrayList<>();    // thread-safe, best for read-heavy operations
+        ListIterator<Integer> integerListIterator = list.listIterator();
+        if (integerListIterator.hasNext()){
+            System.out.println(integerListIterator.next());
+        }
+        Serializable serializable = integerListIterator.hasNext() ? integerListIterator.next() : 0;
+
+        Iterator<Integer> iterator=list1.iterator();
+        int i = iterator.hasNext() ? iterator.next() : 0;
+        //Iterator can only move forward and remove elements.
+        //ListIterator can move forward & backward and can add, set, remove elements.
+
+        //Iterator<Integer> it = list.iterator(); --------- Fail fast iterator
+        //list.add(3);          // modification
+        //it.next();            // Exception here
+
+        // For fail safe iteration use concurrent / copy on arraylist etc.
+
+        int[] arr=new int[10];
+        List<Integer> list5 = List.of(arr[0]);//immutable
+        List<Integer> list6 = new ArrayList<>(Arrays.asList(arr[0]));//mutable
+
+
 
 //------------ Copy-On-Write Strategy ------------
 //When a write operation happens (add, set, remove):
@@ -36,6 +57,19 @@ public class CollectionFramework {
         Set<Integer> set3 = new TreeSet<>();                  // sorted (Red-Black tree), no nulls
         Set<Integer> set4 = new CopyOnWriteArraySet<>();      // thread-safe, internally uses CopyOnWrite mechanism
         Set<Integer> set5 = new ConcurrentSkipListSet<>();    // thread-safe, sorted, scalable concurrency
+
+        //hashCode + equals: Used to detect duplicates in sets
+        //SortedSet: Maintains sorted order
+        //NavigableSet: Adds navigation methods
+        //Null in Set: Depends on implementation, trees doesn't allow null, hashset allows one
+
+        Set<MyEnum> set = new HashSet<>();
+        set.add(new MyEnum(1));
+        set.add(new MyEnum(1));   // added again! thats why we need !!!! overriding of hashcode
+//refer below custom class
+
+
+
 
         // ----- Queue Implementations -----
 
@@ -88,11 +122,44 @@ public class CollectionFramework {
         Map<Integer, String> m7 = new WeakHashMap<>();               // keys eligible for GC (weak references)
         Map<Integer, String> m8 = new IdentityHashMap<>();           // key comparison using == instead of equals()
         Map<Integer, String> m9 = new EnumMap<>(MyEnum.class);       // fast map for enum keys
+//!!! map is separate interface, as its having k,v pairs. rest of the collection framework work values are single valued
+        //those methods like .keyset, .values would not fit the collections framework, soo
 
+        //Role of Generics:List<Integer>, List<String> etc.
+        //Generics provide compile-time type safety and eliminate runtime ClassCastException in collections.
+        //Why Generics Help:
+        //They reduce bugs, remove casting, and make code safer and more readable.
+        //Raw Collections:List list=new ArrayList<>();
+        //Raw collections are non-generic collections that lack type safety and can cause runtime errors.
+        //Homogeneous vs Heterogeneous:List<Integer> vs List<Object>
+        //Homogeneous collections store a single type and are type-safe, while heterogeneous collections store multiple types and are not type-safe.
     }
 
     static class MyEnum {
-        Integer key1;
+        Integer id;
+        MyEnum(Integer ele){
+            this.id=ele;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            return ((MyEnum)o).id == this.id;
+        }//makes sure uniqness
+
+        @Override
+        public int hashCode() {
+            return id;
+        }
+
+        class Child extends MyEnum {
+            Child(Integer ele) {
+                super(ele);
+            }
+
+//            @Override    here we get compile time error as method doesnt exist but we want to override
+            void shoow() {}   // ❌ compile-time error
+        }
+        //Without @Override → no error, method just becomes a new method.
 
     }
 }
